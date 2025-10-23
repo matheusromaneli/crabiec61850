b1d58e9690d853df3ee6c2dde22d2c8e32b21ff8
Matheus Romaneli <mlima1324@gmail.com>
2025-10-23 10:40:38 -0300

    test: to bytes in all structures to guarantee first solution that bytes are created ok

--- src/sampled_values/asdu.rs
+++ src/sampled_values/asdu.rs
@@ -83,91 +83,110 @@ impl Asdu {
         }
     }
 
-    fn sv_id_to_bytes(bytes: &mut Vec<u8>, sv_id: &str) {
+    fn sv_id_to_bytes(sv_id: &str) -> Vec<u8>{
+        let mut bytes: Vec<u8> = vec![];
         bytes.push(ASDUTags::SvId as u8);
         let sv_id_bytes = sv_id.as_bytes();
         bytes.push(sv_id_bytes.len() as u8);
         bytes.append(&mut sv_id_bytes.to_vec());
+        bytes
     }
 
-    fn dataset_to_bytes(bytes: &mut Vec<u8>, dataset: &str) {
+    fn dataset_to_bytes(dataset: &str) -> Vec<u8>{
+        let mut bytes: Vec<u8> = vec![];
         bytes.push(ASDUTags::Dataset as u8);
         let dataset_bytes = dataset.as_bytes();
         bytes.push(dataset_bytes.len() as u8);
         bytes.append(&mut dataset_bytes.to_vec());
+        bytes
     }
 
-    fn smp_count_to_bytes(bytes: &mut Vec<u8>, smp_count: u16) {
+    fn smp_count_to_bytes(smp_count: u16) -> Vec<u8>{
+        let mut bytes: Vec<u8> = vec![];
         bytes.push(ASDUTags::SmpCount as u8);
         bytes.push(2);
         bytes.append(&mut vec![0; 2]);
         BigEndian::write_u16(&mut bytes[2..], smp_count);
+        bytes
     }
 
-    fn conf_rev_to_bytes(bytes: &mut Vec<u8>, conf_rev: u32) {
+    fn conf_rev_to_bytes(conf_rev: u32) -> Vec<u8>{
+        let mut bytes: Vec<u8> = vec![];
         bytes.push(ASDUTags::ConfRev as u8);
         bytes.push(4);
         bytes.append(&mut vec![0; 4]);
         BigEndian::write_u32(&mut bytes[2..], conf_rev);
+        bytes
     }
 
-    fn refr_tm_to_bytes(bytes: &mut Vec<u8>, refr_tm: u64) {
+    fn refr_tm_to_bytes(refr_tm: u64) -> Vec<u8>{
+        let mut bytes: Vec<u8> = vec![];
         bytes.push(ASDUTags::RefrTm as u8);
         bytes.push(8);
         bytes.append(&mut vec![0; 8]);
         BigEndian::write_u64(&mut bytes[2..], refr_tm);
+        bytes
     }
 
-    fn smp_sync_to_bytes(bytes: &mut Vec<u8>, smp_sync: u8) {
+    fn smp_sync_to_bytes(smp_sync: u8) -> Vec<u8>{
+        let mut bytes: Vec<u8> = vec![];
         bytes.push(ASDUTags::SmpSync as u8);
         bytes.push(1);
         bytes.push(smp_sync);
+        bytes
     }
 
-    fn smp_rate_to_bytes(bytes: &mut Vec<u8>, smp_rate: u16) {
+    fn smp_rate_to_bytes(smp_rate: u16) -> Vec<u8>{
+        let mut bytes: Vec<u8> = vec![];
         bytes.push(ASDUTags::SmpRate as u8);
         bytes.push(2);
-        BigEndian::write_u16(bytes, smp_rate);
+        BigEndian::write_u16(&mut bytes[2..], smp_rate);
+        bytes
     }
 
-    fn measures_to_bytes(bytes: &mut Vec<u8>, measures: &Phases) {
+    fn measures_to_bytes(measures: &Phases) -> Vec<u8>{
+        let mut bytes: Vec<u8> = vec![];
         bytes.push(ASDUTags::Measures as u8);
         bytes.push(64);
         bytes.append(&mut measures.to_bytes());
+        bytes
     }
 
-    fn smp_mode_to_bytes(bytes: &mut Vec<u8>, smp_mode: u16) {
+    fn smp_mode_to_bytes(smp_mode: u16) -> Vec<u8>{
+        let mut bytes: Vec<u8> = vec![];
         bytes.push(ASDUTags::SmpMode as u8);
         bytes.push(2);
-        BigEndian::write_u16(bytes, smp_mode);
+        bytes.append(&mut vec![0; 2]);
+        BigEndian::write_u16(&mut bytes[2..], smp_mode);
+        bytes
     }
 
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut bytes: Vec<u8> = vec![];
-        Self::sv_id_to_bytes(&mut bytes, &self.sv_id);
+        bytes.append(&mut Self::sv_id_to_bytes(&self.sv_id));
 
         if let Some(dataset) = &self.dataset {
-            Self::dataset_to_bytes(&mut bytes, dataset);
+            bytes.append(&mut Self::dataset_to_bytes(dataset));
         }
 
-        Self::smp_count_to_bytes(&mut bytes, self.smp_count);
+        bytes.append(&mut Self::smp_count_to_bytes(self.smp_count));
 
-        Self::conf_rev_to_bytes(&mut bytes, self.conf_rev);
+        bytes.append(&mut Self::conf_rev_to_bytes(self.conf_rev));
 
         if let Some(refr_tm) = &self.refr_tm {
-            Self::refr_tm_to_bytes(&mut bytes, *refr_tm);
+            bytes.append(&mut Self::refr_tm_to_bytes(*refr_tm));
         }
 
-        Self::smp_sync_to_bytes(&mut bytes, self.smp_sync as u8);
+        bytes.append(&mut Self::smp_sync_to_bytes(self.smp_sync as u8));
 
         if let Some(smp_rate) = &self.smp_rate {
-            Self::smp_rate_to_bytes(&mut bytes, *smp_rate);
+            bytes.append(&mut Self::smp_rate_to_bytes(*smp_rate));
         }
 
-        Self::measures_to_bytes(&mut bytes, &self.measures);
+        bytes.append(&mut Self::measures_to_bytes(&self.measures));
 
         if let Some(smp_mode) = &self.smp_mode {
-            Self::smp_mode_to_bytes(&mut bytes, *smp_mode);
+            bytes.append(&mut Self::smp_mode_to_bytes(*smp_mode));
         }
 
         bytes
@@ -208,35 +227,26 @@ mod tests {
 
     #[test]
     fn add_sv_id() {
-        let mut bytes: Vec<u8> = vec![];
-        Asdu::sv_id_to_bytes(&mut bytes, "4000");
-        assert_eq!(bytes, vec![0x80, 0x04, 0x34, 0x30, 0x30, 0x30]);
+        assert_eq!(Asdu::sv_id_to_bytes("4000"), vec![0x80, 0x04, 0x34, 0x30, 0x30, 0x30]);
     }
 
     #[test]
     fn add_smp_count() {
-        let mut bytes: Vec<u8> = vec![];
-        Asdu::smp_count_to_bytes(&mut bytes, 0);
-        assert_eq!(bytes, vec![0x82, 0x02, 0x00, 0x00]);
+        assert_eq!(Asdu::smp_count_to_bytes(0), vec![0x82, 0x02, 0x00, 0x00]);
     }
     
     #[test]
     fn add_conf_rev() {
-        let mut bytes: Vec<u8> = vec![];
-        Asdu::conf_rev_to_bytes(&mut bytes, 1);
-        assert_eq!(bytes, vec![0x83, 0x04, 0x00, 0x00, 0x00, 0x01]);
+        assert_eq!(Asdu::conf_rev_to_bytes(1), vec![0x83, 0x04, 0x00, 0x00, 0x00, 0x01]);
     }
 
     #[test]
     fn add_smp_sync() {
-        let mut bytes: Vec<u8> = vec![];
-        Asdu::smp_sync_to_bytes(&mut bytes, SampleSync::Local as u8);
-        assert_eq!(bytes, vec![0x85, 0x01, 0x01]);
+        assert_eq!(Asdu::smp_sync_to_bytes(SampleSync::Local as u8), vec![0x85, 0x01, 0x01]);
     }
 
     #[test]
     fn add_measures() {
-        let mut bytes: Vec<u8> = vec![];
         let measures = Phases {
             current: PhaseMeasures {
                 a: PhaseMeasurement{ value: 0, quality: 0},
@@ -251,9 +261,25 @@ mod tests {
                 n: PhaseMeasurement{ value: 0, quality: 0},
             },
         };
-        Asdu::measures_to_bytes(&mut bytes, &measures);
+        
         let mut expected: Vec<u8> = vec![0x87, 0x40];
         expected.append(&mut vec![0; 64]);
-        assert_eq!(bytes, expected);
+        assert_eq!(Asdu::measures_to_bytes(&measures), expected);
     }
+
+    #[test]
+    fn asdu_to_bytes(){
+        let bytes: &[u8] = &[
+            0x80, 0x04, 0x34, 0x30, 0x30, 0x30, 0x82, 0x02, 0x00, 0x00, 0x83, 0x04, 0x00, 0x00,
+            0x00, 0x01, 0x85, 0x01, 0x01, 0x87, 0x40, 0xff, 0xff, 0xff, 0xfd, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x03, 0x00, 0x00, 0x20, 0x00, 0xff, 0xff, 0xff, 0xfd, 0x00, 0x00, 0x00, 0x00, 0xff,
+            0xff, 0xff, 0xfd, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xfc, 0x00, 0x00, 0x00, 0x00, 0xff,
+            0xff, 0xff, 0xf6, 0x00, 0x00, 0x20, 0x00,
+        ];
+
+        let object = Asdu::from_bytes(bytes);
+        assert_eq!(object.to_bytes(), bytes);
+    }
+
 }
