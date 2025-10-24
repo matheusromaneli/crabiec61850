27b00e9b88035474dd526d75ec70384cdaaa0740
Matheus Romaneli <mlima1324@gmail.com>
2025-10-24 10:16:15 -0300

    chore: create mods

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod network;
+pub mod publisher;
+pub mod sampled_values;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-use crate::sampled_values::model::SampledValue;
-
-pub mod sampled_values;
-
 fn main() {
-    SampledValue::from_bytes(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]);
+
 }
